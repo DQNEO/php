5a7102ca36f8b313ac88078df5a93f2b4922e5a1
DQNEO <dqneoo@gmail.com>
2015-03-20 00:08:48 +0900

    libcurl-devel is required for extension

--- phpbuild/build.sh
+++ phpbuild/build.sh
@@ -4,7 +4,7 @@ set -e
 
 yum -y update
 yum -y install wget tar make gcc autoconf bison re2c
-yum -y install libxml2-devel
+yum -y install libxml2-devel libcurl-devel
 yum -y install git
 yum clean all
 
