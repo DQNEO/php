9616feb7ff37276eaf716859a882e4dcf19b3dbc
DQNEO <dqneoo@gmail.com>
2015-03-19 23:28:22 +0900

    php build on CentOS7

--- phpbuild/build.sh
+++ phpbuild/build.sh
@@ -5,6 +5,13 @@ set -e
 yum -y update
 yum -y install wget tar make gcc autoconf bison re2c
 yum -y install libxml2-devel
+yum -y install git
 yum clean all
+
+git clone git://github.com/php/php-src
+cd php-src
+./buildconf
+./configure
+
 
 
